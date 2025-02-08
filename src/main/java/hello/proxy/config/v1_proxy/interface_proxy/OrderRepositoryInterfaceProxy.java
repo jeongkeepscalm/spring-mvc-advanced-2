dc852ce377d491ef9b4ce067d53bda8926f9196f
JEONGGIL\ojg01 <jeongkeepscalm@gmail.com>
2025-02-08 15:41:03 +0900

    프록시

---
+++ src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -0,0 +1,30 @@
+package hello.proxy.config.v1_proxy.interface_proxy;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 이전 OrderRepositoryImpl 에 추가해야했었던 로직을 프록시를 사용하여 분리한다.
+ */
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+
+    private final OrderRepositoryV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.save()");
+            //target 호출
+            target.save(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
